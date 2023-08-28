6ff341076167d0aff2e37e4b93898a10052db37d
gwadholm <gwadholm@gmail.com>
2023-08-28 19:08:26 -0400

    Fixed age alert

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let lastName = "Wadholm";
 let age = "102";
 
 alert("My name is " + firstName + " " + lastName + ".");
-alert("I am " + age + ".");
+alert("I am " + age + "years old.");
 
 console.log("I am having tons of fun in this class! I've been meaning to learn Javascript for forever, so this is a good challenge for me.");

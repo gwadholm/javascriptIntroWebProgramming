35d302a393f06d0b4fd0c8202d7c02e90480991e
gwadholm <gwadholm@gmail.com>
2023-08-28 19:06:02 -0400

    Fixed errors

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// datatypes:
- 
-// String
-// Use consistent quotes for strings, single or double
- 
-// Numbers
-// JS doesn't distinguish between int and floating point
-
-// Booleans
-// True or False 
- 
-// Empty
-// undefined or null 
-// null is purposely assigned, undefined is not defined by you
-
-// Video 2
-// Built in JS functions
-// alert
-// prompt()
-// console.log()
-// 
-// 
-// 
-// 
-
-// var let or const to store a value
-// declaration plus assignment
-
-
-// alert("Hello World")
-
 let firstName = "Grace";
 let lastName = "Wadholm";
 let age = "102";

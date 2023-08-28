9177539f79bb77c48c980c74894c0be8af0ddeb7
gwadholm <gwadholm@gmail.com>
2023-08-28 19:03:36 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// datatypes:
+ 
+// String
+// Use consistent quotes for strings, single or double
+ 
+// Numbers
+// JS doesn't distinguish between int and floating point
+
+// Booleans
+// True or False 
+ 
+// Empty
+// undefined or null 
+// null is purposely assigned, undefined is not defined by you
+
+// Video 2
+// Built in JS functions
+// alert
+// prompt()
+// console.log()
+// 
+// 
+// 
+// 
+
+// var let or const to store a value
+// declaration plus assignment
+
+
+// alert("Hello World")
+
+let firstName = "Grace";
+let lastName = "Wadholm";
+let age = "102";
+
+alert("My name is " + firstName + " " + lastName + ".");
+alert("I am " + age + ".");
+
+console.log("I am having tons of fun in this class! I've been meaning to learn Javascript for forever, so this is a good challenge for me.");
